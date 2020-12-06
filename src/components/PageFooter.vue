ac89df13ff1d356c4b8710f935b4a7c77300ea4f
Freyberth <freyberth_96@hotmail.com>
2020-12-05 21:01:21 -0500

    Envio del footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- inicio del footer -->
+    <footer class="page-footer">
+      <!-- Inicio dos columnas pie de pagina -->
+      <div class="container-fluid bg-secondary text-white">
+        <div class="row">
+          <!-- columna   1 -->
+          <div class="col-md footer-col text-left">
+
+            <h6 class="text-uppercase small font-weight-bold">Integrantes</h6>
+            <ul class="list-group-flush small text-left">
+
+              <li class ="list-group-item bg-secondary text-white text-left">José Luis Muñoz: dlufyace@gmail.com</li>
+              <li class ="list-group-item bg-secondary text-white text-left">Néstor Manosalvab: nemanosalvab@unal.edu.co</li>
+              <li class ="list-group-item bg-secondary text-white text-left" >Andrés Espejo: jefferson.espejo@gmail.com</li>
+              <li class ="list-group-item bg-secondary text-white text-left">Fabián Díaz: fasadif@yahoo.com</li>
+              <li class ="list-group-item bg-secondary text-white text-left">Freyberth Pabón: freyberth_96@gmail.com</li>
+            </ul>
+          </div>
+          <!-- columna 2 -->
+          <div class="col-md footer-col text-left">
+
+            <h6 class="text-uppercase small font-weight-bold">
+              Mantengase Conectado
+            </h6>
+
+            <ul
+              class="list-group list-group-flush small text-left"
+            >
+              <li class="list-group-item bg-secondary text-white text-left">
+                Suscribirse
+              </li>
+              <li class="list-group-item bg-secondary text-white">
+                Avisos de privacidad
+              </li>
+              <li class="list-group-item bg-secondary text-white">
+                Terminos y condiciones
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- fin dos columnas -->
+      </div>
+      <!--Inicio declaracion de copyright  -->
+      <div class="container-fluid bg-dark text-white">
+        <p class="small text-center">©2020 Copyright MinTIC-Grupo-50</p>
+      </div>
+
+      <!-- fin declaracion de copyright -->
+    </footer>
+    <!-- fin del footer -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageFooter",
+};
+</script>
